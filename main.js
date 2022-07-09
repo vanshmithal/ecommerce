26b3aa1f6bb8b7df720824a7d7c7435999f95cec
vansh-mithal <vanshmithal@gmail.com>
2022-07-09 17:37:05 +0530

    url changes

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const hostname = '0.0.0.0'
 const port = 3000
 
 //db initialized!
 require('./database')
 
 //ejs templating
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 //get models
 var userModel = require('./database/models/userModel.js')
@@ -19,17 +20,19 @@ const getProducts = require('./utils/productsDB')
 
 //express-session module
 var session = require('express-session')
-app.use(session({
-  secret: '123456789',
-  resave: false,
-  saveUninitialized: true,
-}))
+app.use(
+  session({
+    secret: '123456789',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 //middlewares
-app.use( express.static('public') )
-app.use( express.static('uploads') )
-app.use( express.urlencoded() )
-app.use( express.json() )
+app.use(express.static('public'))
+app.use(express.static('uploads'))
+app.use(express.urlencoded())
+app.use(express.json())
 
 const adminRoute = require('./routes/admin')
 app.use('/admin', adminRoute)
@@ -41,95 +44,84 @@ const signupRoute = require('./routes/signup')
 app.use('/signup', signupRoute)
 
 app.get('/signout', (req, res) => {
-	
-	req.session.fromSignout = req.session.user ? req.session.user.username: 'server refresh...'
-	res.redirect('/login')
-
-} )
+  req.session.fromSignout = req.session.user
+    ? req.session.user.username
+    : 'server refresh...'
+  res.redirect('/login')
+})
 
 app.get('/getNextProducts/:id', (req, res) => {
-	const {id} = req.params
-	
-	getProducts(null , null, (data) => {
-		if( data ){
-			if(id === 'full'){
-				res.end(JSON.stringify(data))
-			}
-			else{
-				var end = ~~id+5 < data.length ? ~~id+5 : data.length+1
-				var tempData = data.slice( id, end  )
-				res.end(JSON.stringify(tempData))
-			}
-		}
-		else{
-			res.status(500)
-			res.end('some internal error!')
-		}
-	})
-	
+  const { id } = req.params
+
+  getProducts(null, null, (data) => {
+    if (data) {
+      if (id === 'full') {
+        res.end(JSON.stringify(data))
+      } else {
+        var end = ~~id + 5 < data.length ? ~~id + 5 : data.length + 1
+        var tempData = data.slice(id, end)
+        res.end(JSON.stringify(tempData))
+      }
+    } else {
+      res.status(500)
+      res.end('some internal error!')
+    }
+  })
 })
 
 app.get('/verifyUser/:id', (req, res) => {
+  const { id } = req.params
 
-	const { id } = req.params
-	
-	userModel.findOne( { _id: id })
-		.then( data => {
-
-			if( data ){
-
-				if( data.verified ){
-						req.session.fromVerify = data.username
-						res.redirect('/login')
-				}
-				else{
-					userModel.updateOne({ _id: id }, { verified: true}, (err, d) => {
-						if(err){
-							res.status(500)
-							res.end('internal server error')
-						}
-						else{
-							req.session.fromVerify = data.username
-							res.redirect('/login')
-						}
-					});
-				}
-
-			}
-			else{
-				res.status(500)
-				res.end('not in our records!')
-			}
-		})
-		.catch( () => {
-			res.status(500)
-			res.end('some internal error occured')
-		})
-
+  userModel
+    .findOne({ _id: id })
+    .then((data) => {
+      if (data) {
+        if (data.verified) {
+          req.session.fromVerify = data.username
+          res.redirect('/login')
+        } else {
+          userModel.updateOne({ _id: id }, { verified: true }, (err, d) => {
+            if (err) {
+              res.status(500)
+              res.end('internal server error')
+            } else {
+              req.session.fromVerify = data.username
+              res.redirect('/login')
+            }
+          })
+        }
+      } else {
+        res.status(500)
+        res.end('not in our records!')
+      }
+    })
+    .catch(() => {
+      res.status(500)
+      res.end('some internal error occured')
+    })
 })
 
 const forgotPassRoute = require('./routes/forgotPass')
 app.use('/forgotPassword', forgotPassRoute)
 
-app.get( '/resetPassword/:id', (req, res) => {
-	var { id } = req.params
+app.get('/resetPassword/:id', (req, res) => {
+  var { id } = req.params
 
-	userModel.findOne( { _id: id } )
-		.then( data => {
-			if( data ){
-				req.session.resetPass = data.username
-				res.redirect('/resetPassword')
-			}
-			else{
-				res.status(401)
-				res.end('not in our records!')
-			}
-		})
-		.catch( data => {
-			res.status(500)
-			res.end('some internal error occured!')
-		})
-
+  userModel
+    .findOne({ _id: id })
+    .then((data) => {
+      if (data) {
+        req.session.resetPass = data.username
+        res.redirect('/resetPassword')
+      } else {
+        res.status(401)
+        res.end('not in our records!')
+      }
+    })
+    .catch((data) => {
+      res.status(500)
+      res.end('some internal error occured!')
+    })
 })
 
 const resetPassRoute = require('./routes/resetPass')
@@ -139,26 +131,24 @@ const shoppingCartRoute = require('./routes/shoppingCart')
 app.use('/shoppingCart', shoppingCartRoute)
 
 app.get('/getUserCart', (req, res) => {
-	if(req.session.isLoggedIn){
-		res.status(200)
-		getUserCartDB( req.session.user._id, ( data =>{
-			if( data ){
-				res.end(JSON.stringify({ data: JSON.stringify(data) }))
-			}
-			else{
-				res.end('error fetching user cart')
-			}
-		}) )
-	}
-	else{
-		res.status(401)
-		res.end('unauthorised access')
-	}
+  if (req.session.isLoggedIn) {
+    res.status(200)
+    getUserCartDB(req.session.user._id, (data) => {
+      if (data) {
+        res.end(JSON.stringify({ data: JSON.stringify(data) }))
+      } else {
+        res.end('error fetching user cart')
+      }
+    })
+  } else {
+    res.status(401)
+    res.end('unauthorised access')
+  }
 })
 
 const rootRoute = require('./routes/root')
 app.use('/', rootRoute)
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, hostname, () => {
+  console.log(`Example app listening at http://${hostname}:${port}`)
 })
